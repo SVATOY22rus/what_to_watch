7c98caeedcd293e22eea4fa4c255b2d061de4799
Andrey Lukyanenko <svatoy.a@yandex.ru>
2025-12-01 23:14:57 +0300

    pre relise

--- settings.py
+++ settings.py
@@ -1,5 +1,7 @@
 import os
 
+
 class Config(object):
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
-    SECRET_KEY = os.getenv('SECRET_KEY') 
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    DROPBOX_TOKEN = os.getenv('DROPBOX_TOKEN')
